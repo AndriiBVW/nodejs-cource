a47dadf459bafb2e38ad5096eca9ac6ceb4638f2
AndriiBVW <andriibvw@gmail.com>
2026-04-03 15:54:22 +0300

    feat: add user authentication routes for login and registration

--- express/index.js
+++ express/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { router as usersRouter } from "./users/users.js";
 
 const PORT = 3000;
 const app = express();
@@ -6,6 +7,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/users", usersRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
