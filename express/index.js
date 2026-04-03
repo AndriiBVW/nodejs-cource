f36e8b8fa8eb1fe52685bbdf413e67b2ab3347a6
AndriiBVW <andriibvw@gmail.com>
2026-04-03 17:00:14 +0300

    feat: add middleware for logging request time and error handling

--- express/index.js
+++ express/index.js
@@ -4,11 +4,25 @@ import { router as usersRouter } from "./users/users.js";
 const PORT = 3000;
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(`Time: ${Date.now()}`);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/err", (req, res) => {
+  throw new Error("Error!");
+});
+
 app.use("/users", usersRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(401).send(err.message);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
