fe25e9eadbda844f028a5bbc0b4cb9378957810d
AndriiBVW <andriibvw@gmail.com>
2026-04-06 18:24:09 +0300

    feat: add LoggerService for structured logging and integrate into app initialization

---
+++ express/src/logger/logger.service.ts
@@ -0,0 +1,23 @@
+import { ILogObj, Logger } from "tslog";
+
+export class LoggerService {
+  public logger: Logger<ILogObj>;
+
+  constructor() {
+    this.logger = new Logger({
+      hideLogPositionForProduction: true,
+    });
+  }
+
+  log(...args: unknown[]) {
+    this.logger.info(...args);
+  }
+
+  error(...args: unknown[]) {
+    this.logger.error(...args);
+  }
+
+  warn(...args: unknown[]) {
+    this.logger.warn(...args);
+  }
+}
